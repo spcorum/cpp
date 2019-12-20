bf5771e8958931fbad7e1340922f684b4fd29e0a
spcorum <spcorum@gmail.com>
2019-12-19 22:58:07 -0600

    Initial stab at Chapter 7 Drill

---
+++ stroustrup/ch7/drill/my.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<string>
+#include<vector>
+#include<algorithm>
+#include<cmath>
+#include "my.h"
+
+using namespace std;
+
+inline void keep_window_open() {char ch; cin>>ch;}
+
+void print_foo() {
+  cout << foo << endl;
+}
+
+void print(int i) {
+  cout << i << endl;
+}
